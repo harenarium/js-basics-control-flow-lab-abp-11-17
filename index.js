f558842ca990ec18c7adbe9129212eb84dca78aa
harenarium <carolinel2610@gmail.com>
2017-11-09 03:13:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your code in this file!
+function scuberGreetingForFeet(feet){
+  if(feet<=400){
+    return 'This one is on me!'
+  }else if(feet>2000){
+    return 'I will gladly take your thirty bucks.'
+  }else if(feet>2500){
+    return 'No can do.'
+  }
+  
+} 
+
+
+function ternaryCheckCity(city){
+  return city==='NYC' ? "Ok, sounds good." : "No go."
+} 
+
+
+function switchOnCharmFromTip(quality){
+  switch(quality){
+    case 'generous':
+      return "Thank you so much."
+    case 'not as generous':
+      return "Thank you."
+    default:
+      return 'bye'
+
+  }
+} 
+
+
+  it('should return generous' if anything else', function () {
+    expect(switchOnCharmFromTip('thanks for everything')).to.equal('Bye.');
+  });
+});
+});
+
+*/
